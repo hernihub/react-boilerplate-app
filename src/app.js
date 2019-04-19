84d345b6a2d746d13dd6b72d2986384cebd3797d
hernihub <hernan_quevedo@yahoo.com>
2019-04-19 17:58:20 +1000

    first commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+ReactDOM.render(<p>This is a boilterplate React project</p>, document.getElementById('app'));
